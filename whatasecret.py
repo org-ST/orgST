a080506bf28311bbd7ce234e145d0f8251637a0a
Wdboyes13 <modes-recheck-0r@icloud.com>
2024-12-17 13:27:13 -0800

    changed orgST_alpha.py to use variables from maindata.json changed orgST_alpha.py to restart program on end changed orgST_alpha.py to use variables form randdata.json in supertext

--- whatasecret.py
+++ whatasecret.py
@@ -1,3 +1,3 @@
-a=input("input supertext"):
+a = input("input supertext")
 if a=="Will, replace this with the JSON supertext code":
-  print("you cracked the code congrats"
+  print("you cracked the code congrats")

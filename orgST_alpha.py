4f51bfaa83907a2f0a6e95f824dc4c1693c39a25
Wdboyes13 <modes-recheck-0r@icloud.com>
2024-12-17 10:54:54 -0800

    Update orgST_alpha.py

--- orgST_alpha.py
+++ orgST_alpha.py
@@ -12,37 +12,43 @@ with open('maindata.json', 'r') as file:
     jsonfile = json.load(file)
 
 def restart_program():
-    """Restarts the current program.
-    Note: this function does not return. Any cleanup action (like
-    saving data) must be done before calling this function."""
     python = sys.executable
     os.execl(python, python, * sys.argv)
 
 
 def main():
     if a==supertext:
-        print("licence,creativecommons,history,git,credits,sauce,sponsors,org
+        print("holder")
+        
     if a=="licence" or "creativecommons":
-        print("orgST © 2024 by Wdboyes13, progman.task is licensed under Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International. To view a copy of this license, visit https://creativecommons.org/licenses/by-nc-nd/4.0/"
+        print(jsonfile["license"])
+   
     if a=="history":
         print("version ", jsonfile["version"])
+        
     if a=="git":
         print(jsonfile["github"])
+        
     if a=="credits":
         print(jsonfile["authors"])
+        
     if a=="sauce":
         print("dump raw data?")
         d=input("Y/N")
         if d=="Y" or "Yes" or "yes":
             print("dumping...)
+                  
     if a=="sponsors":
         print("current sponsors")
         print(jsonfile["sponsornames"])
+        
     if a=="org":
         print("would you like to start orgST setup?")
         print("Y/N")
         b=input("input command= ")
         if b=="Y" or "Yes" or "yes":
-           print("support=287j48ah
+           print("support=287j48ah")
+
+    
     restart_program()
 main()

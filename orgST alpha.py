b05bdb8b38e583b8690d142a5de69684b9c04cac
progman.task <makidevhelp@gmail.com>
2024-12-16 21:26:00 -0800

    first ver of orgST
    written in idle

---
+++ orgST alpha.py
@@ -0,0 +1,10 @@
+print("orgST alpha build")
+print("!DO NOT RUN THIS IN FOLDER TERMINAL!")
+print("refer to help manual to get started")
+a=input("input command= ")
+if a=="history":
+    print("version 1.0")
+if a=="github":
+    print("https://github.com/MakiDevelops/orgST")
+if a=="credits":
+    print("progman.task (makidev), Wdboyes13")

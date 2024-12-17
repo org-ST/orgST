f2e111d597c42f3f030524d9eeeb007482303317
Wdboyes13 <modes-recheck-0r@icloud.com>
2024-12-17 09:53:49 -0800

    changed orgST alpha.py to use variables from maindata.json and changed orgST alpha.py to restart program on end

--- macosLP.py
+++ macosLP.py
@@ -1,5 +1,3 @@
-MACOS
-
 print("orgST patcher MAC")
 print("terminal folder ver.")
 print("would you like to patch orgST?")
--- orgST alpha.py
+++ orgST alpha.py
@@ -1,26 +1,48 @@
+import json
+import sys
+import os
+
 print("orgST alpha build")
 print("!DO NOT RUN THIS IN FOLDER TERMINAL!")
 print("refer to help manual to get started")
+
 a=input("input command= ")
-if a=="history":
-    print("version 1.0")
-if a=="github":
-    print("https://github.com/MakiDevelops/orgST")
-if a=="credits":
-    print("progman.task (makidev), Wdboyes13")
-if a=="sauce":
-    print("you found the sauce!")
-if a=="org":
-    print("would you like to start orgST setup?")
-    print("Y/N")
-    b=input("input command= ")
-    if b=="Y" or "Yes" or "yes":
-        print("searching for data.ini")
-        print("data.ini not found")
-        print("are you using MACOS?")
+
+with open('maindata.json', 'r') as file:
+    jsonfile = json.load(file)
+
+def restart_program():
+    """Restarts the current program.
+    Note: this function does not return. Any cleanup action (like
+    saving data) must be done before calling this function."""
+    python = sys.executable
+    os.execl(python, python, * sys.argv)
+
+
+def main():
+    if a=="history":
+        print("version ", jsonfile["version"])
+    if a=="github":
+        print(jsonfile["github"])
+    if a=="credits":
+        print(jsonfile["authors"])
+    if a=="sauce":
+        print("you found the sauce!")
+    if a=="sponsors":
+        print(jsonfile["sponsornames"])
+    if a=="org":
+        print("would you like to start orgST setup?")
         print("Y/N")
-        c=input("input command= ")
-        if c=="Y" or "Yes" or "yes":
-            print("MACOS is currently not supported lmao")
-        if c=="N" or "No" or "no":
-            print("sorry, there is nothing we can do")
+        b=input("input command= ")
+        if b=="Y" or "Yes" or "yes":
+            print("searching for data.ini")
+            print("data.ini not found")
+            print("are you using MACOS?")
+            print("Y/N")
+            c=input("input command= ")
+            if c=="Y" or "Yes" or "yes":
+                print("MACOS is currently not supported lmao")
+            if c=="N" or "No" or "no":
+                print("sorry, there is nothing we can do")
+    restart_program()
+main()

7fc41169084cb7cdb2c9a24133e1f6cb664f08a3
progman.task <makidevhelp@gmail.com>
2024-12-17 21:09:29 -0800

    Update orgST_alpha.py

--- orgST_alpha.py
+++ orgST_alpha.py
@@ -5,9 +5,9 @@ import random
 
 print("orgST pre-alpha terminal 1.1")
 print("refer to the help manual for more information")
-supertexterr="poilo"
+supertexterr="poilo" # what is this?
 
-a = input("Enter your choice: ")
+a = input("orgST: ")
 
 with open('maindata.json', 'r') as file:
    jsonfile = json.load(file)
@@ -16,46 +16,57 @@ def restart_program():
     python = sys.executable
     os.execl(python, python, * sys.argv)
 
+# Get the supertext
 with open('randdata.json', 'r') as randfile:
     randdata = json.load(randfile)
 def main():
     if a=="getsuper":
         rand = [randfile["b"], randfile["c"], randfile["d"], randfile["e"], randfile["f"]]
         print(random.choice(rand))
-        
+
+   # Check licence
     if a=="lis" or "CC":
         print(jsonfile["license"])
-   
+
+   # Check upd. history
     if a=="his":
         print("version ", jsonfile["version"])
-        
+
+   # Check the github link
     if a=="git":
         print(jsonfile["github"])
-        
+
+   # Check credits
     if a=="cred":
         print(jsonfile["authors"])
 
+   # Check the eufi version (aka the start command's version it's downloading
     if a=="eufi":
        print(jsonfile["eufi"])
 
-   
+   # Dumps the raw json data files
     if a=="sauce":
         print("dump raw data?")
         d=input("Y/N")
         if d=="Y" or "Yes" or "yes":
             print("dumping...")
-                  
+
+   # Check sponsor listings
     if a=="spon":
         print("current sponsors")
         print(jsonfile["sponsornames"])
-        
+
+   # download the org files with UI
+    if a=="orgGET":
+       
+
+   
     if a=="org":
        print("have you viewed the current supertext?")
        print("press * to continue, otherwise press /")
        i3 = input("...")
        if i3=="*":
           print("insert your supertext")
-      
     
     restart_program()
 main()

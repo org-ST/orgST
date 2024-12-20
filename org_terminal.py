12bb899f875bf41631afbc30206dff8f683687c8
Wdboyes13 <modes-recheck-0r@icloud.com>
2024-12-19 21:34:13 -0800

    Update org_terminal.py
    Added error handling and changed getsuper to super also removing org and modifying super to include the function of org to make sure that the correct value gets changed

--- org_terminal.py
+++ org_terminal.py
@@ -9,65 +9,66 @@ print("refer to the help manual for more information")
 supertexterr="poilo"
 
 a = input("Enter your choice: ")
+try{
+   with open('maindata.json', 'r') as file:
+      jsonfile = json.load(file)
 
-with open('maindata.json', 'r') as file:
-   jsonfile = json.load(file)
+   def restart_program():
+       python = sys.executable
+       os.execl(python, python, * sys.argv)
 
-def restart_program():
-    python = sys.executable
-    os.execl(python, python, * sys.argv)
+   with open('randdata.json', 'r+') as json_file:
+       randdata = json.load(json_file)
+   def main():
+       keys = ["b", "c", "d", "e", "f"]
+       randchoice = random.choice(keys)
 
-with open('randdata.json', 'r+') as json_file:
-    randdata = json.load(json_file)
-def main():
-    keys = ["b", "c", "d", "e", "f"]
-    randchoice = random.choice(keys)
-
-    if a=="getsuper":
-        print(randdata[randchoice])
+       if a=="super":
+           print(randdata[randchoice])
+           print("Would you like to update the supertext")
+           i3 = input("Enter y to change the supertext: ")
+           if i3=="Y" or "y" or "Yes" or "yes":
+              with open('randdata.json', 'w') as file:
+                 intext = input("insert your supertext: ")
+                 randdata.update({randchoice: intext})
+                 json.dump(randdata, file, indent=6)
         
-    if a=="lis" or "CC":
-        print(jsonfile["license"])
+       if a=="lis" or "CC":
+           print(jsonfile["license"])
+      
+       if a=="his":
+           print("version ", jsonfile["version"])
+           
+       if a=="git":
+           print("github link: ", jsonfile["github"])
+           
+       if a=="cred":
+           print("authors: ", jsonfile["authors"])
    
-    if a=="his":
-        print("version ", jsonfile["version"])
-        
-    if a=="git":
-        print("github link: ", jsonfile["github"])
-        
-    if a=="cred":
-        print("authors: ", jsonfile["authors"])
-
-    if a=="eufi":
-       print(jsonfile["eufi"])
-
+       if a=="eufi":
+          print(jsonfile["eufi"])
    
-    if a=="sauce":
-        print("dump raw data?")
-        d=input("Y/N")
-        if d=="Y" or "Yes" or "yes":
-            print(jsonfile)
-
-                  
-    if a=="spon":
+      
+       if a=="sauce":
+           print("dump raw data?")
+           d=input("Y/N")
+           if d=="Y" or "Yes" or "yes":
+               print(jsonfile)
+   
+                     
+       if a=="spon":
         print("current sponsors")
         print(jsonfile["sponsornames"])
         
-    if a=="org":
-       print("have you viewed the current supertext?")
-       print("press * to continue, otherwise press /")
-       i3 = input("...")
-       if i3=="*":
-           with open('randdata.json', 'w') as file:
-              intext = input("insert your supertext: ")
-              randdata.update({randchoice: intext})
-              json.dump(randdata, file, indent=6)
-
-    if a=="esc":
-        return None
-
-    if a =="start":
-        subprocess.run(["python3", "app.py"])
-        return None
-    restart_program()
+   
+       if a=="esc":
+           return None
+   
+       if a =="start":
+           subprocess.run(["python3", "app.py"])
+           return None
+    except:
+       restart_program()
+    finally:
+       restart_program()
 main()

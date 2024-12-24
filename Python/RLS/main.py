743519c6f79a5788fd9e777f2f9a7fcd5b3cfefe
Wdboyes13 <modes-recheck-0r@icloud.com>
2024-12-23 19:58:40 -0800

    added deoendencies to install scripts

--- Python/RLS/main.py
+++ Python/RLS/main.py
@@ -1,5 +1,4 @@
 import json
-import sys
 import os
 import random
 import subprocess
@@ -19,60 +18,61 @@ def restart_program():
     python = sys.executable
     os.execl(python, python, * sys.argv)
 
+
 with open('JSONs/randdata.json', 'r+') as json_file:
     randdata = json.load(json_file)
+
+
 def main():
     keys = ["b", "c", "d", "e", "f"]
     randchoice = random.choice(keys)
 
-    if a=="super":
+    if a == "super":
         print(randdata[randchoice])
         print(" Would you like to update the supertext")
         i3 = input(" Enter y to change the supertext: ")
-        if i3=="Y" or "y" or "Yes" or "yes":
+        if i3 == "Y" or "y" or "Yes" or "yes":
             with open('JSONs/randdata.json', 'w') as file:
                 intext = input(" insert your supertext: ")
                 randdata.update({randchoice: intext})
                 json.dump(randdata, file, indent=6)
 
-      
-    if a=="his":
+    if a == "his":
         print(" version ", jsonfile["version"])
-           
-    if a=="git":
+
+    if a == "git":
         print(" github link: ", jsonfile["github"])
-           
-    if a=="cred":
+
+    if a == "cred":
         print(" authors: ", jsonfile["authors"])
-   
-    if a=="eufi":
+
+    if a == "eufi":
         print(jsonfile["eufi"])
-   
-      
-    if a=="sauce":
+
+    if a == "sauce":
         print(" Would you like to dump raw data?")
-        d=input(" Y/N: ")
-        if d=="Y":
+        d = input(" Y/N: ")
+        if d == "Y":
             print(jsonfile)
-   
-                     
-    if a=="channel":
+
+    if a == "channel":
         subprocess.run(["python3", "PYextras/channelviewer.py"])
         return None
 
-   
-    if a=="esc":
+    if a == "esc":
         return None
-   
-    if a =="start":
+
+    if a == "start":
         subprocess.run(["python3", "app.py"])
         return None
 
-    if a =="aero":
+    if a == "aero":
         print(" Aero is not supported for this TERMINAL release.")
 
-    if a =="pride":
+    if a == "pride":
         subprocess.run(["python3", "PYextras/TheFlag.py"])
 
     restart_program()
+
+
 main()

4322f76e589bb6b10fe975edd42c0b546e1e0739
Chureki <andydang16122011@gmail.com>
2025-02-21 17:27:38 -0800

    Remade channelviewer.py
    - instead of running solely on hardcoded channels, all channel data is in channeldata.json, and viewer pulls from it

--- PYextras/channelviewer.py
+++ PYextras/channelviewer.py
@@ -1,122 +1,177 @@
 # Channel Viewer without UI
-# Created by progman.task
+# Created by progman.task and Table
 
-import time
+from time import sleep
 import os
-import subprocess
+from subprocess import run
 import webbrowser
+from random import choice as rnd_choice
+import json
 
-rev = 2.0
-sponsors = 4
-secureaccesscode = 293
 is_looping = True
+intro = False
 
-print("Gathering Channels...")
-time.sleep(2)
-print("Gathering Sponsor Listings...")
-time.sleep(3)
-print("Sponsors:")
-print(sponsors)
-print("Revisions:")
-print(rev)
-print("Your secure access code is 293")
-print("")
-print("")
+os.system('clear')
 
+with open('JSONs/channeldata.json', 'r') as file:
+    data = json.load(file)
+    # print(data)
 
+if intro:
+    print("\rGathering Channels", end = '')
+    sleep(0.5)
+    print("\rGathering Channels.", end = '')
+    sleep(0.5)
+    print("\rGathering Channels..", end = '')
+    sleep(0.5)
+    print("\rGathering Channels...")
+    sleep(0.5)
+    print("\rGathering Sponsor Listings", end = '')
+    sleep(0.5)
+    print("\rGathering Sponsor Listings.", end = '')
+    sleep(0.5)
+    print("\rGathering Sponsor Listings..", end = '')
+    sleep(0.5)
+    print("\rGathering Sponsor Listings...", end = '')
+    sleep(0.5)
 
+# code = int(str(rnd_choice(range(10))) + str(rnd_choice(range(10))) + str(rnd_choice(range(10))))
+# print("Your secure access code is " + str(code))
+# print("")
 
-print("Channel Viewer")
+yes_ins = ['y','yes','Yes','Y','check','Check']
+no_ins = ['n','no','No','N']
+
+def do_help():
+    print("commands: check, products, people, website, info, comment")
+
+print("Channel Viewer V2.0")
 print("Would you like to check the current channels?")
 a = input(">...")
-while is_looping:
-    if a == "Y":
+if yes_ins.count(a):
+    while True:
         print("Listings:")
-        print("1. The OrgST Public Channel")
-        print("2. The CalcTech Channel")
-        print("3. toyathing's MMD Channel")
-        print("4. The Archive Channel")
+        for n in range(len(data["channels"])):
+            print(str(n + 1) + '. ' + data[data["channels"][n]]["name"])
+        
+        print('(input \"help\" at any time for a list of commands!)')
         b = input(">...")
-        if b == "1":
-            print("Greetings. Welcome to the OrgST Public Channel.")
-            print("-progman.task")
-            c = input(">...")
+        try:
+            channel = data[data['channels'][int(b) - 1]]
+        except:
+            if b == 'help':
+                do_help()
+                sleep(2)
+                continue
+            else:
+                print('Error: invalid input, please input the number corresponding to the channel')
+                sleep(2)
+                continue
+        
+        while True:
+            print('Greetings. Welcome to ' + channel['name'] + '.')
+            c = input('>...')
             if c == "check":
-                print("We've compiled some interesting things for you to look at.")
-                print("Please choose one of the options below:")
-                print("1. ")
-            if c == "web":
-                webbrowser.open('satelliteearth.wordpress.com')
-            if c == "purchase":
-                print("This channel is empty. Please check this at a later date.")
-            if c == "review":
-                print("There is nothing to review.")
-            if c == "people":
-                print("Wdboyes13, @kaycutier (progman.task)")
-            if c == "info":
-                print("The public orgST channel.")
-                print("Date created: 2024-12-21")
-        if b == "2":
-            print("Greetings. Welcome to the CalcTech Channel.")
-            c = input(">...")
-            if c == "check":
-                print("Found 1 result")
-                print("Opening calctrash.py")
-                time.sleep(3)
-                subprocess.run(["python3", "PYextras/calctrash.py"])
-            if c == "web":
-                print("There is no website for CalcTech.")
-            if c == "purchase":
-                print("There is nothing to purchase.")
-            if c == "review":
-                print("There is nothing to review.")
-            if c == "people":
-                print("@chureki (Table), @kaycutier (progman.task)")
-            if c == "info":
-                print("A channel for CalcTech!")
-                print("Date created: 2024-12-21")
-        if b == "3":
-            print("Greetings. Welcome to the toyathing Channel.")
-            c = input(">...")
-            if c == "check":
-                print("This channel is empty. Please check this at a later date.")
-            if c == "web":
-                print("This channel is empty. Please check this at a later date.")
-            if c == "purchase":
-                print("This channel is empty. Please check this at a later date.")
-            if c == "review":
-                print("There is nothing to review.")
-            if c == "people":
-                print("toyathing")
-            if c == "info":
-                print("A channel for MMD!")
-                print("Date created: 2024-12-21")
-        if b == "4":
-            print("Greetings. Welcome to the Archive Channel.")
-            c = input(">...")
-            if c == "check":
-                print("This channel is empty. Please check this at a later date.")
-            if c == "web":
-                print("This channel is empty. Please check this at a later date.")
-            if c == "purchase":
-                print("This channel is empty. Please check this at a later date.")
-            if c == "review":
-                print("There is nothing to review.")
-            if c == "people":
-                print("progman.task, Wdboyes13, @chureki, toyathing")
-            if c == "info":
-                print("A channel that archives ANYTHING orgST. If something's missing, contact us!")
-                print("Date created: 2024-12-21")
+                files = channel['files']
+                if files == False:
+                    print("There are no files associated with this channel.")
+                    sleep(2)
+                    continue
+                elif type(files) == list:
+                    print("We found some files for you to look at:")
+                    for n in range(len(files)):
+                        print(str(n + 1) + '. ' + files[n])
+                    d = input('>...')
+                    try:
+                        int(d)
+                        print('Opening ' + files[int(d) - 1] + '...')
+                        sleep(3)
+                        run(['python3', files[int(d) - 1]])
+                    except ValueError:
+                        print('Error: invalid input')
+                        sleep(2)
+                        print('')
+                        continue
+                    except:
+                        print('Error: file doesn\'t seem to exist...')
+                        sleep(2)
+                        print('')
+                        continue
+                    
+            elif c == 'web':
+                site = channel['website']
+                if site == False:
+                    print('There is no website associated with this channel.')
+                    sleep(2)
+                    continue
+                elif type(site) == str:
+                    print('Opening ' + site + '...')
+                    sleep(3)
+                    webbrowser.open(site)
+                
+            elif c == 'products':
+                products = channel['products']
+                if products == False:
+                    print('There are no products associated with this channel')
+                    sleep(2)
+                    continue
+                elif type(products) == list:
+                    print('We found some products for you:')
+                    for n in range(len(products)):
+                        print(str(n + 1) + '. ' + products[n])
+                    d = input('>...')
+                    try:
+                        int(d)
+                        print('Opening ' + products[int(d) - 1] + '...')
+                        sleep(3)
+                        webbrowser.open(products[int(d) - 1] + '...')
+                    except ValueError:
+                        print('Error: invalid input')
+                        sleep(2)
+                        print('')
+                        continue
+                    except:
+                        print('Error: product doesn\'t seem to exist...')
+                        sleep(2)
+                        print('')
+                        continue
+            
+            elif c == 'people':
+                print('People: ' + ', '.join(channel['people']))
+                sleep(2)
+                continue
+            
+            elif c == 'info':
+                print(channel['info'])
+                sleep(2)
+                continue
+            
+            elif c == "comment":
+                print(channel['comment'])
+                sleep(2)
+                continue
+            
+            elif c == "help":
+                do_help()
+                sleep(2)
+                continue
+            
+            elif c == 'exit':
+                break
+            
+        sleep(2)
+        print('')
 
-
-
-        elif is_looping:
+        if not(is_looping):
             break
 
-
-
-if a == "N":
-    subprocess.run(["python3", "main.py"])
-if a == "HELP":
-    print("-check -purchase -review -people -web -info")
+elif no_ins.count(a):
+    run(["python3", "main.py"])
+elif a == 'DON\'T YOU FRICKIN DARE':
+    print('ok man geez')
+    run(["python3", "main.py"])
+elif a == 'edit':
+    run(['python3', 'channelhelper.py'])
+if ['help','Help'].count(a):
+    do_help()
 

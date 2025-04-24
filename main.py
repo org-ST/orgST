7559972a5e6ef8ddf91c43f8744c7c693709a5a0
Wdboyes13 <willdev2025@outlook.com>
2025-04-24 12:42:11 -0700

    added comments related to java porting

--- main.py
+++ main.py
@@ -7,17 +7,31 @@ inp = fn.head()
 
 with open('JSONs/maindata.json', 'r') as file:
     jsonfile = json.load(file)
-
+# if you see ported that means its been succesfully ported to java
+# port: x  is the priority for the command to be ported
 def main():
     match inp:
-        case "orgid": fn.orgid()
-        case "help": print("CMDS: hist, git, sauce, esc, run, super")
-        case "hist": fn.hist()
-        case "git": print(" github link: ", jsonfile["github"])
-        case "sauce": fn.sauce(yes_ins, jsonfile)
-        case "esc": return None
-        case "run": fn.run()
-        case 'pride': fn.pride()
+        case "orgid": fn.orgid() #port: 5
+        case "help": print("CMDS: hist, git, sauce, esc, run, super") # ported
+        case "hist": fn.hist() # ported
+        case "git": print(" github link: ", jsonfile["github"]) #port: 1
+        case "sauce": fn.sauce(yes_ins, jsonfile) # ported
+        case "esc": return None #port: 2
+        case "run": fn.run() #port: 3
+        case 'pride': fn.pride() #port: 4
 
     fn.restart_program()
 main()
+
+"""
+To be Ported:
+   orgid/client.py (part of orgid command)
+   PYextras/channelviewer.py
+   PYextras: TheFlag
+   PYextras/channelapps/calctrash.py
+   Remaining Commands : See ^
+Doesnt need porting:
+    JSONs/* : Will be replaced by Java files containing Variable definitions
+    PYextras/channelapps/apptest.py : Testing automatically done by Gradle org.orgst.AppTast
+     
+"""

72f6fe89fae7c3d5b3ebc007b8a6238f4e4c2665
Wdboyes13 <willdev2025@outlook.com>
2025-04-24 13:31:53 -0700

    Updated Java Port

--- java/app/src/main/java/org/orgst/ChannelViewer.java
+++ java/app/src/main/java/org/orgst/ChannelViewer.java
@@ -5,6 +5,9 @@ public class ChannelViewer {
     String[] no_ins = {"n","no","No","N"};
     public static void help(){
         System.out.println("commands: check, products, people, website, info, comment");
-        
+    }
+    public static void CV() {
+    System.out.println("Channel Viewer V2.0.1");
+    System.out.println("Would you like to check the current channels?");
     }
 }

b41bddf729a635373dc497ed5db191e52b694917
Wdboyes13 <willdev2025@outlook.com>
2025-04-24 16:54:06 -0700

    Updated Java Port

--- java/app/src/main/java/org/orgst/ChannelViewer.java
+++ java/app/src/main/java/org/orgst/ChannelViewer.java
@@ -1,15 +1,14 @@
 package org.orgst;
 
 import java.util.Scanner;
-import java.util.Arrays;
 public class ChannelViewer {
-    
+    String[] yes_ins = {"y","yes","Yes","Y","check","Check"};
+    String[] no_ins = {"n","no","No","N"};
     public static void help(){
         System.out.println("commands: check, products, people, website, info, comment");
     }
     public static void CV(String[] args) {
-        String[] yes_ins = {"y","yes","Yes","Y","check","Check"};
-        String[] no_ins = {"n","no","No","N"};
+
         System.out.println("Channel Viewer V2.0.1");
         System.out.println("Would you like to check the current channels?");
         for (int i = 0; i < ChannelData.Channels.length; i++){

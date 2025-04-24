41ef667337de9b2c03361bd46c77e06117ce02ff
Wdboyes13 <willdev2025@outlook.com>
2025-04-24 15:46:34 -0700

    Updated Java Port

--- java/app/src/main/java/org/orgst/ChannelViewer.java
+++ java/app/src/main/java/org/orgst/ChannelViewer.java
@@ -18,8 +18,9 @@ public class ChannelViewer {
         while (true){
             System.out.print("..> ");
             String inp = inpscanner.nextLine();
+            int inpi = Integer.parseInt(inp);
             try {
-            String channel = ChannelData.Channels[toInt.req(inp)];
+            String channel = ChannelData.Channels[inpi];
             } catch (Exception e) {
                 switch(inp){
                     case "help" : help(); break;

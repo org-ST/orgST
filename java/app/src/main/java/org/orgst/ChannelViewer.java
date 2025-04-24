b9ff83a979f21a69acc05cf442f24b5a7fdcbefc
Wdboyes13 <willdev2025@outlook.com>
2025-04-24 16:48:04 -0700

    Updated Java Port

--- java/app/src/main/java/org/orgst/ChannelViewer.java
+++ java/app/src/main/java/org/orgst/ChannelViewer.java
@@ -26,6 +26,7 @@ public class ChannelViewer {
                 switch(inp){
                     case "help" : help(); break;
                     case "exit" : App.main(args); break;
+                    case "DON'T YOU FRICKIN DARE" : System.out.println("ok man geez");
                 } 
             }
         }

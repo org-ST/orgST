8711e1221eb9b7ae8cfc679abd0d8dc70043358e
Wdboyes13 <willdev2025@outlook.com>
2025-04-24 11:57:53 -0700

    Updated Java Port

--- java/app/src/main/java/org/orgst/Methods.java
+++ java/app/src/main/java/org/orgst/Methods.java
@@ -2,17 +2,19 @@ package org.orgst;
 import java.util.Map;
 import java.util.Scanner;
 public class Methods {
-    public static String head() {
+    public static void head() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
         System.out.println("orgST © 2024-2025 by Wdboyes13, progman.task is licensed under CC BY-NC-ND 4.0\n" + "'orgST', 'organizationSTATION', and the orgST logo are trademarks of orgST.");
         System.out.println("---------------------------------------");
         System.out.println("orgST Terminal 1.8 - Java");
         System.out.println("Refer to the readme for more information.");
+    };
+    public static String input(){
         Scanner inp = new Scanner(System.in);
         String Cmd = inp.nextLine();
         return Cmd;
-    };
+    }
     public static void sauce() {
         for (Map.Entry<String, Vars.Data> entry : Vars.channels.entrySet()) {
         System.out.println("Key: " + entry.getKey());

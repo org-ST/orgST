c1093d8e974372fdd27f7c1120f513a7b2e43def
Wdboyes13 <willdev2025@outlook.com>
2025-04-24 12:28:38 -0700

    Updated Java Port

--- java/app/src/main/java/org/orgst/AppData.java
+++ java/app/src/main/java/org/orgst/AppData.java
@@ -3,8 +3,9 @@ package org.orgst;
 import java.util.HashMap;
 
 public class AppData {
+    static HashMap<String, String> AppData;
     static {
-        HashMap<String, String> AppData = new HashMap<>();
+        AppData = new HashMap<>();
         AppData.put("Version", "1.8.0");
         AppData.put("Updates", "9");
         AppData.put("Authors", "progman.task, Wdboyes13, chureki");

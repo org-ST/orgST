7f75da4821513a6430816da0526ad6b32adef48f
Wdboyes13 <willdev2025@outlook.com>
2025-04-24 12:30:39 -0700

    Updated Java Port

--- java/app/src/main/java/org/orgst/Methods.java
+++ java/app/src/main/java/org/orgst/Methods.java
@@ -30,7 +30,7 @@ public class Methods {
     }
     public static void hist(){
         for (Map.Entry<String, String> entry : AppData.AppData.entrySet()){
-            System.out.println(entry.getKey() + ":" + entry.getValue());
+            System.out.println(entry.getKey() + " : " + entry.getValue());
         }
     }
 }

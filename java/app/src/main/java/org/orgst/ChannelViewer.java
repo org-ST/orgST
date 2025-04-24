75fe4a30861e0e51b9d6562cd1f033bb0ac6d6dd
Wdboyes13 <willdev2025@outlook.com>
2025-04-24 13:32:10 -0700

    Updated Java Port

--- java/app/src/main/java/org/orgst/ChannelViewer.java
+++ java/app/src/main/java/org/orgst/ChannelViewer.java
@@ -1,5 +1,7 @@
 package org.orgst;
 
+import java.util.Scanner;
+
 public class ChannelViewer {
     String[] yes_ins = {"y","yes","Yes","Y","check","Check"};
     String[] no_ins = {"n","no","No","N"};

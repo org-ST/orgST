bdd3fbb86762ee3f3c7009f5d7a9439d379fd308
Wdboyes13 <willdev2025@outlook.com>
2025-04-24 22:04:04 -0700

    Updated Java Port

--- java/app/src/main/java/org/orgst/OrgID/Client.java
+++ java/app/src/main/java/org/orgst/OrgID/Client.java
@@ -2,30 +2,25 @@ package org.orgst.OrgID;
 import java.security.*;
 import java.security.spec.*;
 import javax.crypto.spec.*;
+
 import javax.crypto.*;
 import java.util.Base64;
 import java.nio.charset.StandardCharsets;
 import java.security.interfaces.*;
+import java.io.InputStream;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class Client {
-    public static String encrypt(String password, PublicKey publicKey) throws Exception {
-        Cipher cipher = Cipher.getInstance("RSA/ECB/OAEPWithSHA-256AndMGF1Padding");
-        OAEPParameterSpec oaepParams = new OAEPParameterSpec(
-            "SHA-256",
-            "MGF1",
-            MGF1ParameterSpec.SHA256,
-            PSource.PSpecified.DEFAULT
-        );
+    public static void submit(String url, String username, String password){
+        try {
+        PublicKey pub = KeyLoader.loadPublicKey();
 
-        cipher.init(Cipher.ENCRYPT_MODE, publicKey, oaepParams);
-        byte[] encryptedBytes = cipher.doFinal(password.getBytes(StandardCharsets.UTF_8));
-        return Base64.getEncoder().encodeToString(encryptedBytes); // optional: encode to string
-    }
-    public static void submit(String url){
-        
+        String passw = Encrypt.encrypt(password, pub);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }

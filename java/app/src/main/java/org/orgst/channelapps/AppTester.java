e9b7487a527c8f0ece2eb0adcb3761d582b75c23
Wdboyes13 <willdev2025@outlook.com>
2025-04-24 19:13:54 -0700

    Updated Java Port

--- java/app/src/main/java/org/orgst/channelapps/AppTester.java
+++ java/app/src/main/java/org/orgst/channelapps/AppTester.java
@@ -1,5 +1,13 @@
 package org.orgst.ChannelApps;
-
+import java.util.Scanner;
 public class AppTester {
-    
+    public static void start(){
+        Scanner input = new Scanner(System.in);
+        while (true){
+            String inp = input.nextLine();
+            switch(inp) {
+                
+            }       
+        }
+    }
 }

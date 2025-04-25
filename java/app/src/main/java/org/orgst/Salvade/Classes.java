c954b4f7bc5f01cb1af80a3ee9c128d06cf820b5
Wdboyes13 <willdev2025@outlook.com>
2025-04-25 08:59:08 -0700

    Updated Java Port

--- java/app/src/main/java/org/orgst/Salvade/Classes.java
+++ java/app/src/main/java/org/orgst/Salvade/Classes.java
@@ -7,19 +7,26 @@ public class Classes {
         String[] skills;
         String[] dialogue;
         int aggro;
-        int[] hp = {stats[0], stats[0]};
-        int[] sheild = {stats[1], stats[1]};
-        int[] mana = {stats[2], stats[2]};
-        int attack = stats[3];
+        int[] hp;
+        int[] sheild;
+        int[] mana;
+        int attack;
         int crit = 25;
         int[] statuses = {0, 0, 0, 0, 0};
         int broken = 0;
+
         public enemy(String name, int[] stats, String[] skills, String[] dialogue, int aggro) {
             this.name = name;
             this.stats = stats;
             this.skills = skills;
             this.dialogue = dialogue;
             this.aggro = aggro;
+
+            this.hp = new int[] { stats[0], stats[0] };
+            this.sheild = new int[] { stats[1], stats[1] };
+            this.mana = new int[] { stats[2], stats[2] };
+            this.attack = stats[3];
         }
     }
+    
 }

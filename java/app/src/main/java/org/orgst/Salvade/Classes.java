bd2d2f7c91b7e17232f7f223b6035d971d8e5f87
Wdboyes13 <willdev2025@outlook.com>
2025-04-25 08:51:44 -0700

    Updated Java Port

--- java/app/src/main/java/org/orgst/Salvade/Classes.java
+++ java/app/src/main/java/org/orgst/Salvade/Classes.java
@@ -4,6 +4,10 @@ public class Classes {
     public class enemy {
         String name;
         int[] stats;
-        
+        String[] skills;
+        String[] dialogue;
+        public enemy(String name, int[] stats, String[] skills, String[] dialogue) {
+            
+        }
     }
 }

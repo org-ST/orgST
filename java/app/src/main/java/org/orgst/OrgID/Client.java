12bfe04814bd0d007c656654c5ec7e76c46c3374
Wdboyes13 <willdev2025@outlook.com>
2025-04-24 22:04:49 -0700

    Updated Java Port

--- java/app/src/main/java/org/orgst/OrgID/Client.java
+++ java/app/src/main/java/org/orgst/OrgID/Client.java
@@ -17,8 +17,8 @@ public class Client {
     public static void submit(String url, String username, String password){
         try {
         PublicKey pub = KeyLoader.loadPublicKey();
-
         String passw = Encrypt.encrypt(password, pub);
+        String usr = Encrypt.encrypt(username, pub);
         } catch (Exception e){
             e.printStackTrace();
         }

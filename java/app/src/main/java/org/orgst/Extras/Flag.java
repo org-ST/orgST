442ff7d974d340260e58e5b2d115bdafcab20dd0
Wdboyes13 <willdev2025@outlook.com>
2025-04-25 08:05:42 -0700

    Updated Java Port

--- java/app/src/main/java/org/orgst/Extras/Flag.java
+++ java/app/src/main/java/org/orgst/Extras/Flag.java
@@ -1,16 +1,24 @@
 package org.orgst.Extras;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-
+import javax.swing.*;
+import java.io.*;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
 public class Flag {
     public static void start(){
-     JFrame frame = new JFrame("My Nerdy Window");
-        JButton button = new JButton("Click me!");
-
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(300, 200);
-        frame.getContentPane().add(button); // add button to frame
-        frame.setVisible(true);
+        try {
+            InputStream is = Flag.class.getResourceAsStream("/flag.png");
+            BufferedImage img = ImageIO.read(is);
+            JFrame frame = new JFrame("Pride");
+            ImageIcon flag = new ImageIcon(img);
+            JLabel flagLabel = new JLabel(flag);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(300, 200);
+            frame.getContentPane().add(flagLabel); // add button to frame
+            frame.setVisible(true);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        
     }
 }

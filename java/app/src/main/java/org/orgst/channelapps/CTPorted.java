f9d9ecb44491823b622d2a6f3a284595763b160f
Wdboyes13 <willdev2025@outlook.com>
2025-04-24 23:51:46 -0700

    Updated Java Port

--- java/app/src/main/java/org/orgst/channelapps/CTPorted.java
+++ java/app/src/main/java/org/orgst/channelapps/CTPorted.java
@@ -3,38 +3,6 @@ import java.util.*;
 import org.orgst.Variables.Ops;
 public class CTPorted {
     public static Float[] ans;
-    public static Object take_input(Boolean special, String prompt){
-        try {
-            System.out.println(prompt);
-        } catch (Exception e){System.out.println("ProgramError: expected string in arg prompt"); return false;}
-        Scanner inp = new Scanner(System.in);
-        String a = inp.nextLine();
-        try {
-            Float af = Float.parseFloat(a);
-            return af;
-        } catch(Exception e) {
-            if (a.equals("lop") && special == true){
-                try {
-                    System.out.print("How many operations back? \n >>");
-                    String c = inp.nextLine();
-                    int ci = Integer.parseInt(c);
-                    Float af = ans[ans.length - ci*2];
-                    return af;
-                } catch (ArrayIndexOutOfBoundsException g){
-                    System.out.println("Error: there was no lop to get");
-                    return false;
-                } catch (Exception f){
-                    System.out.println("Error: invalid value");
-                }
-            } else if (a.equals("cancel") && special == true){
-                System.out.println("Operation Canceled");
-                return false;
-            } else {
-                System.out.println("Error: please input a number");
-            }
-        }
-        return false;
-    }
     public static boolean check_input(Object to_check){
         if (to_check instanceof Integer || to_check instanceof Float) {
             return true;

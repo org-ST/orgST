cd95d5efbab0b73942cfb46142ee4f2a1cdb9e08
Wdboyes13 <willdev2025@outlook.com>
2025-04-25 01:38:13 -0700

    Updated Java Port

--- java/app/src/main/java/org/orgst/Extras/Flag.java
+++ java/app/src/main/java/org/orgst/Extras/Flag.java
@@ -1,4 +1,16 @@
 package org.orgst.Extras;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
 public class Flag {
+    public static void start(){
+     JFrame frame = new JFrame("My Nerdy Window");
+        JButton button = new JButton("Click me!");
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 200);
+        frame.getContentPane().add(button); // add button to frame
+        frame.setVisible(true);
+    }
 }

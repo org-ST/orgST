4a04cea57de76b54f4372aa135450ce9d1aab3cb
Wdboyes13 <willdev2025@outlook.com>
2025-04-24 22:48:06 -0700

    Updated Java Port

--- java/app/src/main/java/org/orgst/channelapps/CTPorted.java
+++ java/app/src/main/java/org/orgst/channelapps/CTPorted.java
@@ -1,7 +1,34 @@
 package org.orgst.ChannelApps;
-
+import java.util.*;
 public class CTPorted {
+    public static Float[] ans;
     public static void start(){
     System.out.println("CalcTrash V1.0.3 by @Table");
+    }
+    public static Object take_input(Boolean special, String prompt){
+        try {
+            System.out.println(prompt);
+        } catch (Exception e){System.out.println("ProgramError: expected string in arg prompt"); return false;}
+        Scanner inp = new Scanner(System.in);
+        String a = inp.nextLine();
+        try {
+            Float af = Float.parseFloat(a);
+            return af;
+        } catch(Exception e) {
+            if (a.equals("lop") && special == true){
+                try {
+                    System.out.print("How many operations back? \n >>");
+                    String c = inp.nextLine();
+                    int ci = Integer.parseInt(c);
+                    Float af = ans[ans.length - ci*2];
+                } catch (ArrayIndexOutOfBoundsException g){
+                    System.out.println("Error: there was no lop to get");
+                    return false;
+                } catch (Exception f){
+                    System.out.println("Error: invalid value");
+                }
+            }
+        }
+        return false;
     }
 }

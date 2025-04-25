31dfea9db2aa9a5514ee3707bd69ae1f905b4c31
Wdboyes13 <willdev2025@outlook.com>
2025-04-24 18:19:01 -0700

    Updated Java Port

---
+++ java/app/src/main/java/org/orgst/Extras/PythonRunnerSimple.java
@@ -0,0 +1,15 @@
+package org.orgst.Extras;
+import java.io.*;
+
+public class PythonRunnerSimple {
+    public static void main(String[] args) {
+        try {
+            ProcessBuilder pb = new ProcessBuilder("python3", "app.py");
+            pb.inheritIO(); // makes it use your terminal I/O directly
+            Process process = pb.start();
+            process.waitFor(); // wait for the process to finish
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}

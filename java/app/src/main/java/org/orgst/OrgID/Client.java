4fa1cfeb155063e118c053ba9f9970e3f4bdfa9b
Wdboyes13 <willdev2025@outlook.com>
2025-04-24 22:04:57 -0700

    Updated Java Port

--- java/app/src/main/java/org/orgst/OrgID/Client.java
+++ java/app/src/main/java/org/orgst/OrgID/Client.java
@@ -1,17 +1,5 @@
 package org.orgst.OrgID;
 import java.security.*;
-import java.security.spec.*;
-import javax.crypto.spec.*;
-
-import javax.crypto.*;
-import java.util.Base64;
-import java.nio.charset.StandardCharsets;
-import java.security.interfaces.*;
-import java.io.InputStream;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 
 public class Client {
     public static void submit(String url, String username, String password){

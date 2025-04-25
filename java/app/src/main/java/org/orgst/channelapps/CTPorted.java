d401f9349e2cb0a3a504fc28488e7947c9e0d090
Wdboyes13 <willdev2025@outlook.com>
2025-04-25 00:30:01 -0700

    Updated Java Port

--- java/app/src/main/java/org/orgst/channelapps/CTPorted.java
+++ java/app/src/main/java/org/orgst/channelapps/CTPorted.java
@@ -33,7 +33,27 @@ public class CTPorted {
                         case "multiply": out = Double.toString(Double.parseDouble(opTD[1]) * Double.parseDouble(opTD[2])); break;
                         case "divide": out = Double.toString(Double.parseDouble(opTD[1]) / Double.parseDouble(opTD[2])); break;
                         case "exponentiate": out = Double.toString(Math.pow(Double.parseDouble(opTD[1]), Double.parseDouble(opTD[2]))); break;
-                        case "squareRoot": out = Double.toString(Math.sqrt(Double.parseDouble(opTD[1])));
+                        case "squareRoot": out = Double.toString(Math.sqrt(Double.parseDouble(opTD[1]))); break;
+                        case "lastOp":
+                            if (ans != null && ans.length > 0) {
+                                Scanner lopScanner = new Scanner(System.in);
+                                System.out.println("How many operations back?");
+                                try {
+                                    int idx = Integer.parseInt(lopScanner.nextLine());
+                                    int ansIndex = ans.length - idx;
+                                    if (ansIndex >= 0 && ansIndex < ans.length) {
+                                        System.out.println("Value: " + ans[ansIndex][1]); // or whatever part of the result you want
+                                    } else {
+                                        System.out.println("Error: that far back isn't stored.");
+                                    }
+                                } catch (Exception e) {
+                                    System.out.println("Invalid input.");
+                                }
+                            } else {
+                                System.out.println("No previous operation available.");
+                            }
+                            break;
+
                     }
                     if (out != null){
                         System.out.println(out);

2aef15df78a9409373af5b3330f3f540a44d6efd
Wdboyes13 <willdev2025@outlook.com>
2025-04-25 11:37:13 -0700

    Updated Java Port

--- java/app/src/main/java/org/orgst/Extras/RunCalcTrash.java
+++ java/app/src/main/java/org/orgst/Extras/RunCalcTrash.java
@@ -1,6 +1,8 @@
 package org.orgst.Extras;
 
-import java.io.*;
+import java.io.InputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 public class RunCalcTrash {
     public static void start() {
         try {

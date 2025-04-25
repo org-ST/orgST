b793b6b09bb08fc568ddab31c62e2de074e2c35b
Wdboyes13 <willdev2025@outlook.com>
2025-04-25 08:12:39 -0700

    Updated Java Port

--- java/app/src/main/java/org/orgst/Methods.java
+++ java/app/src/main/java/org/orgst/Methods.java
@@ -7,6 +7,7 @@ import org.orgst.Variables.ChannelData;
 public class Methods {
     public static void flag(){
         new Thread(() -> {org.orgst.Extras.Flag.start();}).start();
+        System.out.print("..> ");
     }
     public static void orgid(){
         Scanner inp = new Scanner(System.in);

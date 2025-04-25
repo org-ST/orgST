f591c22b8619279212fb1da4800fb7c53d9ca315
Wdboyes13 <willdev2025@outlook.com>
2025-04-24 22:13:07 -0700

    Updated Java Port

--- java/app/src/main/java/org/orgst/OrgID/Client.java
+++ java/app/src/main/java/org/orgst/OrgID/Client.java
@@ -19,7 +19,9 @@ public class Client {
             .POST(HttpRequest.BodyPublishers.ofString(json))
             .build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        
+
+        System.out.println("Status: " + response.statusCode());
+        System.out.println("Body: " + response.body());
 
         } catch (Exception e){
             e.printStackTrace();

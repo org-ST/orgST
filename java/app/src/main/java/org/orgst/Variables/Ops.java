2d0a5cd31ff99f712344b6b1f20d8e0555aa14fa
Wdboyes13 <willdev2025@outlook.com>
2025-04-24 23:36:32 -0700

    Updated Java Port

--- java/app/src/main/java/org/orgst/Variables/Ops.java
+++ java/app/src/main/java/org/orgst/Variables/Ops.java
@@ -21,6 +21,6 @@ public class Ops {
         new op(new String[] {"add", "sauce"}, new opInfo(1, "add")),
         new op(new String[]{"sub", "subtract"}, new opInfo(2, "subtract")),
         new op(new String[]{"mul", "multiply"}, new opInfo(2, "multiply")),
-        
+        new op(new String[]{"div", "divide"}, new opInfo(0, null))
     };
 }

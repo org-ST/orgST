3b3f2ae74ce9efba877f5e2b23103e8831b69086
Wdboyes13 <willdev2025@outlook.com>
2025-04-24 19:15:51 -0700

    Updated Java Port

--- java/app/src/main/java/org/orgst/channelapps/AppTester.java
+++ java/app/src/main/java/org/orgst/channelapps/AppTester.java
@@ -1,12 +1,13 @@
 package org.orgst.ChannelApps;
 import java.util.Scanner;
 public class AppTester {
-    public static void start(){
+    public static void start(String[] args){
         Scanner input = new Scanner(System.in);
         while (true){
             String inp = input.nextLine();
             switch(inp) {
-                
+                case "help": System.out.println("help, exit, info");
+                case "exit": org.orgst.Extras.ChannelViewer.CV(args);
             }       
         }
     }

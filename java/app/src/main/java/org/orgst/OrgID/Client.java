bf196329bf0575fd7cdfef2467b41110c084a05f
Wdboyes13 <willdev2025@outlook.com>
2025-04-24 21:50:14 -0700

    Updated Java Port

--- java/app/src/main/java/org/orgst/OrgID/Client.java
+++ java/app/src/main/java/org/orgst/OrgID/Client.java
@@ -1,5 +1,28 @@
 package org.orgst.OrgID;
-
+import java.security.*;
+import java.security.spec.*;
+import javax.crypto.Cipher;
+import javax.crypto.spec.OAEPParameterSpec;
+import javax.crypto.spec.PSource;
+import javax.crypto.spec.*;
+import java.util.Base64;
+import java.nio.charset.StandardCharsets;
+import java.security.interfaces.RSAPublicKey;
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
 public class Client {
-    
+    public static String encrypt(String password, PublicKey publicKey) throws Exception {
+        Cipher cipher = Cipher.getInstance("RSA/ECB/OAEPWithSHA-256AndMGF1Padding");
+        OAEPParameterSpec oaepParams = new OAEPParameterSpec(
+            "SHA-256",
+            "MGF1",
+            MGF1ParameterSpec.SHA256,
+            PSource.PSpecified.DEFAULT
+        );
+
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey, oaepParams);
+        byte[] encryptedBytes = cipher.doFinal(password.getBytes(StandardCharsets.UTF_8));
+        return Base64.getEncoder().encodeToString(encryptedBytes); // optional: encode to string
+    }
+
 }

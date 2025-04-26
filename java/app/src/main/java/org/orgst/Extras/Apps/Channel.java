1987a92aba25ee5e1bf16d54cc1d42f2ca039aec
Wdboyes13 <willdev2025@outlook.com>
2025-04-26 11:13:43 -0700

    Updated Java Port

--- java/app/src/main/java/org/orgst/Extras/Apps/Channel.java
+++ java/app/src/main/java/org/orgst/Extras/Apps/Channel.java
@@ -1,41 +1,29 @@
 package org.orgst.Extras.Apps;
-import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.Group;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import org.orgst.Variables.ChannelData;
-public class Channel extends Application {
-    static ChannelData.Data data;
-    @Override
-    public void start(Stage primaryStage) {
+
+public class Channel {
+    public static void Start(ChannelData.Data data) {
+        Stage stage = new Stage(); // create a NEW stage
         Label label = new Label();
         Button name = new Button("Name");
         Button site = new Button("WebSite");
         Button date = new Button("Date");
         Button comm = new Button("Comment");
-        name.setOnAction(e -> {
-            label.setText(data.name);
-        });
-        site.setOnAction(e -> {
-            label.setText(data.website);
-        });
-        date.setOnAction(e -> {
-            label.setText(data.date);
-        });
-        comm.setOnAction(e -> {
-            label.setText(data.comment);
-        });
-        Group root = new Group();
+        
+        name.setOnAction(e -> label.setText(data.name));
+        site.setOnAction(e -> label.setText(data.website));
+        date.setOnAction(e -> label.setText(data.date));
+        comm.setOnAction(e -> label.setText(data.comment));
+        
+        Group root = new Group(label, name, site, date, comm);
         Scene scene = new Scene(root, 300, 200);
-        primaryStage.setScene(scene);
-        primaryStage.setTitle(data.name);
-        primaryStage.show();
-    }
-
-    public static void Start(ChannelData.Data dataarg) {
-        data = dataarg;
-        launch();
+        stage.setScene(scene);
+        stage.setTitle(data.name);
+        stage.show();
     }
 }

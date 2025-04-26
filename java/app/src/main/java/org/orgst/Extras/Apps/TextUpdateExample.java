ad827b6a30502eac78436d2ab4e59bf950346fa2
Wdboyes13 <willdev2025@outlook.com>
2025-04-26 10:51:23 -0700

    Updated Java Port

---
+++ java/app/src/main/java/org/orgst/Extras/Apps/TextUpdateExample.java
@@ -0,0 +1,28 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class TextUpdateExample extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        Label label = new Label("Waiting...");
+        Button button = new Button("Press me");
+
+        button.setOnAction(e -> {
+            label.setText("Button was pressed! 🎉");
+        });
+
+        VBox root = new VBox(10, label, button);
+        Scene scene = new Scene(root, 300, 200);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Text Update Demo");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}

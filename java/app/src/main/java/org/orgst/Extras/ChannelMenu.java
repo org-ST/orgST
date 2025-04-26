e4d5be762906c6aac246bf976d25bae324703ac7
Wdboyes13 <willdev2025@outlook.com>
2025-04-26 10:36:07 -0700

    Updated Java Port

--- java/app/src/main/java/org/orgst/Extras/ChannelMenu.java
+++ java/app/src/main/java/org/orgst/Extras/ChannelMenu.java
@@ -2,12 +2,25 @@ package org.orgst.Extras;
 import org.orgst.Variables.ChannelData;
 import javafx.application.*;
 import javafx.stage.*;
+import javafx.scene.*;
 import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
 public class ChannelMenu extends Application {
-    Button[] ChannelButtons;
+    Button[] buttons;
+    int btnY = 20;
+    int btnX = 20;
     public void start(Stage primStage){
+        Group root = new Group();
         for (int i = 0; i < ChannelData.Channels.length; i++){
-            ChannelButtons[i] = new Button(ChannelData.Channels[i]);
+            buttons[i] = new Button(ChannelData.Channels[i]);
+        }
+                for (int i = 0; i < buttons.length; i++){
+            final int index = i;
+            buttons[i].setOnAction(e -> {});
+            buttons[i].setTranslateX(btnX);
+            buttons[i].setTranslateY(btnY);
+            root.getChildren().add(buttons[i]);
+            btnY += 40;
         }
     }
 }

d21077e06817c8ff82356b968d69e7ece3d03177
Wdboyes13 <willdev2025@outlook.com>
2025-04-27 13:40:46 -0700

    Updated Java Port

--- java/app/src/main/java/org/orgst/Methods.java
+++ java/app/src/main/java/org/orgst/Methods.java
@@ -82,7 +82,7 @@ public class Methods {
     public static void head() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
-        System.out.println("orgST © 2024-2025 by Wdboyes13, progman.task is licensed under CC BY-NC-ND 4.0\nhttps://creativecommons.org/licenses/by-nc-nd/4.0/\n\"orgST\", \"organizationSTATION\", and the orgST logo are trademarks of orgST.\n" + "'orgST', 'organizationSTATION', and the orgST logo are trademarks of orgST.");
+        System.out.println("orgST © 2024-2025 by Wdboyes13, progman.task is licensed under CC BY-NC-ND 4.0\nhttps://creativecommons.org/licenses/by-nc-nd/4.0/\n\"orgST\", \"organizationSTATION\", and the orgST logo are trademarks of orgST.\n");
         System.out.println("-----------------------------------------------------------------------------------------------------------------------------------------------");
         System.out.println("orgST Terminal 1.8 - Java Ported");
         System.out.println("Ported by Wdboyes13");

8b4409f8d8a817eb2c7b06f9e7e0188ee5d29616
Wdboyes13 <willdev2025@outlook.com>
2025-04-27 14:35:23 -0700

    Updated Java Port

--- java/app/src/main/resources/sal.py
+++ java/app/src/main/resources/sal.py
@@ -81,7 +81,7 @@ class battle:
             'exit' : [self.exit, 0]
         }
     def exit(self, attacker, victim):
-        os.system('java -jar *1.8.jar')
+        exit()
     def damage(self, dmg, attacker, victim):
         shield = victim.shield[1]
         hp = victim.hp[1]

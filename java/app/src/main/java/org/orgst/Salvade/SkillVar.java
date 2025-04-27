df61318ec470c25b047410531917aee694678384
Wdboyes13 <willdev2025@outlook.com>
2025-04-27 11:30:30 -0700

    Updated Java Port

---
+++ java/app/src/main/java/org/orgst/Salvade/SkillVar.java
@@ -0,0 +1,11 @@
+package org.orgst.Salvade;
+
+public class SkillVar {
+    public class Skill{
+    String name;
+    Runnable fun;
+    Integer num;
+    public Skill(String name, Runnable run, Integer num) {
+        
+    }}
+}

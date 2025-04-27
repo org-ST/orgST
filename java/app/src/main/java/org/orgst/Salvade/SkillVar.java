e65dee5c84bd9618d01a9e4ad4041f2ba16b792e
Wdboyes13 <willdev2025@outlook.com>
2025-04-27 11:31:26 -0700

    Updated Java Port

--- java/app/src/main/java/org/orgst/Salvade/SkillVar.java
+++ java/app/src/main/java/org/orgst/Salvade/SkillVar.java
@@ -1,11 +1,14 @@
 package org.orgst.Salvade;
 
+import java.util.HashMap;
+
 public class SkillVar {
     public class Skill{
     String name;
     Runnable fun;
-    Integer num;
-    public Skill(String name, Runnable run, Integer num) {
+    int num;
+    public Skill(String name, Runnable run, int num) {
+        HashMap<Runnable, Integer> skillMap = new HashMap<>();
         
     }}
 }

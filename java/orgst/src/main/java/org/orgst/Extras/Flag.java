a0dafdcac4f3d2f3596a17d84c56e060447b207b
Wdboyes13 <willdev2025@outlook.com>
2025-05-03 17:51:07 -0700

    Updated v1.8

--- java/orgst/src/main/java/org/orgst/Extras/Flag.java
+++ java/orgst/src/main/java/org/orgst/Extras/Flag.java
@@ -1,11 +1,11 @@
 package org.orgst.Extras;
 public class Flag {
     public static void main(String[] args) {
-        System.out.println("\033[31m" + "---------------" + "\033[0m");
-        System.out.println("\033[38;5;214m" + "---------------" + "\033[0m");
-        System.out.println("\033[33m" + "---------------" + "\033[0m");
-        System.out.println("\033[32m" + "---------------" + "\033[0m");
-        System.out.println("\033[34m" + "---------------" + "\033[0m");
-        System.out.println("\033[35m" + "---------------" + "\033[0m");
+        System.out.println("\033[31m" + "==============================" + "\033[0m");
+        System.out.println("\033[38;5;214m" + "==============================" + "\033[0m");
+        System.out.println("\033[33m" + "==============================" + "\033[0m");
+        System.out.println("\033[32m" + "==============================" + "\033[0m");
+        System.out.println("\033[34m" + "==============================" + "\033[0m");
+        System.out.println("\033[35m" + "==============================" + "\033[0m");
     }
 }

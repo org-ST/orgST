1e14663f4ae60b35e569c22dd4f3f1980f9445fb
Wdboyes13 <willdev2025@outlook.com>
2025-05-03 17:49:57 -0700

    Updated v1.8

--- java/orgst/src/main/java/org/orgst/Extras/Flag.java
+++ java/orgst/src/main/java/org/orgst/Extras/Flag.java
@@ -1,38 +1,11 @@
 package org.orgst.Extras;
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
-import javafx.application.Platform;
-public class Flag extends Application {
-
-    @Override
-    public void start(Stage primaryStage) {
-        // Load the image from the resources folder
-        Image image = new Image(getClass().getResourceAsStream("/flag.png"));
-
-        // Create an ImageView to display the image
-        ImageView imageView = new ImageView(image);
-
-        // Add the ImageView to the layout (StackPane in this case)
-        StackPane root = new StackPane();
-        root.getChildren().add(imageView);
-
-        // Create the scene and add it to the stage
-        Scene scene = new Scene(root, 300, 250);
-        primaryStage.setTitle("Pride Flag");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-        primaryStage.setOnCloseRequest( event -> {
-        		Platform.exit();
-        		org.orgst.App.main(new String[0]);
-        	}
-        );
-    }
-
+public class Flag {
     public static void main(String[] args) {
-        launch(args);
+        System.out.println("\033[31m" + "---------------" + "\033[0m");
+        System.out.println("\033[38;5;214m" + "---------------" + "\033[0m");
+        System.out.println("\033[33m" + "---------------" + "\033[0m");
+        System.out.println("\033[32m" + "---------------" + "\033[0m");
+        System.out.println("\033[34m" + "---------------" + "\033[0m");
+        System.out.println("\033[35m" + "---------------" + "\033[0m");
     }
 }
